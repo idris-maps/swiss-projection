8b2f9561c10fa70fda6128c72f0c5d467f67f1dc
Anders <idris.maps@gmx.ch>
2020-04-22 11:29:05 +0200

    Add cli

---
+++ bin/lv03ToWgs.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const run = require('./wrapper')
+const { LV03toWGS } = require('../dist/index')
+
+run(LV03toWGS)
---
+++ bin/wgsToLv95.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const run = require('./wrapper')
+const { WGStoLV95 } = require('../dist/index')
+
+run(WGStoLV95)
